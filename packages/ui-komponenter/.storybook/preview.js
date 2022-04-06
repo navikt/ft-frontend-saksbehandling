9fb15e5c7e03169cfbc219a03f3f5bce6798b8ca
Tor Røneid <tor.roneid@nav.no>
2022-04-06 14:35:55 +0200

    Rydd i avhengigheter (#64)

--- packages/ui-komponenter/.storybook/preview.js
+++ packages/ui-komponenter/.storybook/preview.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import '@formatjs/intl-datetimeformat/polyfill-force';
-import '@formatjs/intl-datetimeformat/locale-data/nb';
-import '@formatjs/intl-numberformat/polyfill-force';
-import '@formatjs/intl-numberformat/locale-data/nb';
 import '../../../.storybook/global.less';
 
 export const decorators = [(Story) => <div style={{ margin: '40px'}}><Story/></div>];
