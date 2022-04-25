a1bc4ea71cfaa55dcafc91bdafc4855e9a69cd01
Espen Johansen Velsvik <espen.velsvik@nav.no>
2022-04-25 11:44:15 +0200

    Legger til tabs i fakta-beregning

--- packages/fakta-beregning/src/components/BeregningInfoPanel.tsx
+++ packages/fakta-beregning/src/components/BeregningInfoPanel.tsx
@@ -3,7 +3,7 @@ import { IntlShape } from 'react-intl';
 
 import { VerticalSpacer } from '@navikt/ft-ui-komponenter';
 import {
-  ArbeidsgiverOpplysningerPerId, AlleKodeverk, Beregningsgrunnlag, Aksjonspunkt, Vilkar,
+  ArbeidsgiverOpplysningerPerId, AlleKodeverk, Beregningsgrunnlag, Vilkar, BeregningAvklaringsbehov, Aksjonspunkt,
 } from '@navikt/ft-types';
 import BeregningFaktaAP, {
   BeregningOverstyringAP, AvklarBeregningsaktiviteterAP,
@@ -17,23 +17,32 @@ import SubmitBeregningType from '../typer/SubmitBeregningTsType';
 
 const {
   VURDER_FAKTA_FOR_ATFL_SN,
+  OVERSTYRING_AV_BEREGNINGSAKTIVITETER,
   OVERSTYRING_AV_BEREGNINGSGRUNNLAG,
+  AVKLAR_AKTIVITETER,
 } = FaktaBeregningAksjonspunktCode;
 
 const hasAksjonspunkt = (aksjonspunktKode: string, aksjonspunkter: Aksjonspunkt[]): boolean => aksjonspunkter
   .some((ap) => ap.definisjon === aksjonspunktKode);
+
+const relevanteKoder = [VURDER_FAKTA_FOR_ATFL_SN,
+  OVERSTYRING_AV_BEREGNINGSAKTIVITETER,
+  OVERSTYRING_AV_BEREGNINGSGRUNNLAG,
+  AVKLAR_AKTIVITETER];
 
 type OwnProps = {
   intl: IntlShape
   submitCallback: (aksjonspunktData: AvklarBeregningsaktiviteterAP | OverstyrBeregningsaktiviteterAP
     | BeregningFaktaAP | BeregningOverstyringAP | SubmitBeregningType[]) => Promise<void>;
   readOnly: boolean;
+  avklaringsbehov: BeregningAvklaringsbehov[];
   aksjonspunkter: Aksjonspunkt[];
   vilkar: Vilkar;
   submittable: boolean;
   erOverstyrer: boolean;
   alleKodeverk: AlleKodeverk;
   beregningsgrunnlag?: Beregningsgrunnlag[];
+  aktivtBeregningsgrunnlagIndeks: number;
   arbeidsgiverOpplysningerPerId: ArbeidsgiverOpplysningerPerId;
   setFormData: (data: any) => void;
   formData: AvklarAktiviteterFormValues;
@@ -47,9 +56,11 @@ type OwnProps = {
 const BeregningInfoPanel: FunctionComponent<OwnProps> = ({
   intl,
   readOnly,
+  avklaringsbehov,
   aksjonspunkter,
   submittable,
   submitCallback,
+  aktivtBeregningsgrunnlagIndeks,
   beregningsgrunnlag,
   erOverstyrer,
   alleKodeverk,
@@ -57,37 +68,42 @@ const BeregningInfoPanel: FunctionComponent<OwnProps> = ({
   setFormData,
   formData,
   vilkar,
-}) => (
-  <div>
-    { /* @ts-ignore */ }
-    <AvklareAktiviteterPanel
-      readOnly={readOnly}
-      harAndreAksjonspunkterIPanel={hasAksjonspunkt(VURDER_FAKTA_FOR_ATFL_SN, aksjonspunkter)}
-      submitCallback={submitCallback}
-      submittable={submittable}
-      erOverstyrer={erOverstyrer}
-      aksjonspunkter={aksjonspunkter}
-      alleKodeverk={alleKodeverk}
-      beregningsgrunnlag={beregningsgrunnlag}
-      arbeidsgiverOpplysningerPerId={arbeidsgiverOpplysningerPerId}
-      setFormData={setFormData}
-      formData={formData}
-      vilkår={vilkar}
-    />
-    <VerticalSpacer thirtyTwoPx />
-    { /* @ts-ignore */ }
-    <VurderFaktaBeregningPanel
-      intl={intl}
-      readOnly={readOnly || ((hasAksjonspunkt(OVERSTYRING_AV_BEREGNINGSGRUNNLAG, aksjonspunkter) || beregningsgrunnlag.at(0).erOverstyrtInntekt) && !erOverstyrer)}
-      submitCallback={submitCallback}
-      submittable={submittable}
-      aksjonspunkter={aksjonspunkter}
-      alleKodeverk={alleKodeverk}
-      beregningsgrunnlag={beregningsgrunnlag.at(0)}
-      erOverstyrer={erOverstyrer}
-      arbeidsgiverOpplysningerPerId={arbeidsgiverOpplysningerPerId}
-    />
-  </div>
-);
+}) => {
+  const relevanteLøsbareAvklaringsbehov = avklaringsbehov.filter((ap) => relevanteKoder.some((kode) => kode === ap.definisjon) && ap.kanLoses !== false);
+  const avklarAktiviteterReadOnly = readOnly || ((relevanteLøsbareAvklaringsbehov.length === 0
+    || hasAvklaringsbehov(OVERSTYRING_AV_BEREGNINGSAKTIVITETER, avklaringsbehov)) && !erOverstyrer);
+  const avklarFaktaBeregningReadOnly = readOnly || ((relevanteLøsbareAvklaringsbehov.length === 0
+    || hasAvklaringsbehov(OVERSTYRING_AV_BEREGNINGSGRUNNLAG, avklaringsbehov)) && !erOverstyrer);
+  return (
+    <div>
+      <AvklareAktiviteterPanel
+        readOnly={avklarAktiviteterReadOnly}
+        harAndreAvklaringsbehovIPanel={hasAvklaringsbehov(VURDER_FAKTA_FOR_ATFL_SN, avklaringsbehov)}
+        submitCallback={submitCallback}
+        submittable={submittable}
+        erOverstyrer={erOverstyrer}
+        alleKodeverk={alleKodeverk}
+        aktivtBeregningsgrunnlagIndeks={aktivtBeregningsgrunnlagIndeks}
+        beregningsgrunnlag={beregningsgrunnlag}
+        arbeidsgiverOpplysningerPerId={arbeidsgiverOpplysningerPerId}
+        setFormData={setFormData}
+        formData={formData}
+        vilkår={vilkar}
+      />
+      <VerticalSpacer thirtyTwoPx />
+      <VurderFaktaBeregningPanel
+        intl={intl}
+        readOnly={avklarFaktaBeregningReadOnly}
+        submitCallback={submitCallback}
+        submittable={submittable}
+        aksjonspunkter={aksjonspunkter}
+        alleKodeverk={alleKodeverk}
+        beregningsgrunnlag={beregningsgrunnlag.at(0)}
+        erOverstyrer={erOverstyrer}
+        arbeidsgiverOpplysningerPerId={arbeidsgiverOpplysningerPerId}
+      />
+    </div>
+  );
+};
 
 export default BeregningInfoPanel;
