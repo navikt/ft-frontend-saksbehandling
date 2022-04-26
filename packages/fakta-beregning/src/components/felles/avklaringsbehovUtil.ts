9f98ac0700a36648c4106c43ea33173f02e2ef7a
Espen Johansen Velsvik <espen.velsvik@nav.no>
2022-04-26 15:32:51 +0200

    Prøver å få til validering

--- packages/fakta-beregning/src/components/felles/avklaringsbehovUtil.ts
+++ packages/fakta-beregning/src/components/felles/avklaringsbehovUtil.ts
@@ -1,8 +1,7 @@
 import { BeregningAvklaringsbehov } from '@navikt/ft-types';
-import AksjonspunktStatus from '@navikt/ft-kodeverk/src/aksjonspunktStatus';
+import { aksjonspunktStatus } from '@navikt/ft-kodeverk';
 
 export const hasAvklaringsbehov = (avklarinsbehovKode: string, avklaringsbehov: BeregningAvklaringsbehov[]): boolean => avklaringsbehov
   .some((ap) => ap.definisjon === avklarinsbehovKode);
 
-
-export const isAvklaringsbehovOpen = (statusKode: string): boolean => statusKode === AksjonspunktStatus.OPPRETTET;
+export const isAvklaringsbehovOpen = (statusKode: string): boolean => statusKode === aksjonspunktStatus.OPPRETTET;
