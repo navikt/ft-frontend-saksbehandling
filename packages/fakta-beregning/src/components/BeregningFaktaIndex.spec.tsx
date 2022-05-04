24111a220041fc99943a44829e866a447206b6ee
Hallvard Andreas Stark <hallvard@scel.to>
2022-05-04 13:55:27 +0200

    Skrevet test for feilmelding vet bytte av tab

--- packages/fakta-beregning/src/components/BeregningFaktaIndex.spec.tsx
+++ packages/fakta-beregning/src/components/BeregningFaktaIndex.spec.tsx
@@ -1,5 +1,5 @@
 import { composeStories } from '@storybook/testing-react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import * as stories from '../BeregningFaktaIndex.stories';
@@ -28,5 +28,38 @@ describe('<BeregningFaktaIndexSpec', () => {
     const feltetMåFyllesUtfeilmelding = await screen.findByText('Feltet må fylles ut');
     expect(måHaAktivitetFeilmelding).toBeInTheDocument();
     expect(feltetMåFyllesUtfeilmelding).toBeInTheDocument();
+  });
+
+  it('skal beholde feilmelding dersom man bytter tab', async () => {
+    render(<ArbeidOgDagpenger />);
+    userEvent.click(screen.getByLabelText('Ikke benytt BEDRIFT AS (910909088) 03.02.2019 til 14.02.2020'));
+    userEvent.click(screen.getByLabelText('Ikke benytt Dagpenger 03.02.2019 til 11.11.2019'));
+    userEvent.click(screen.getByRole('button', { name: 'Oppdater' }));
+    const måHaAktivitetFeilmeldingFørBytteAvTab = await screen.findByText(
+      'Må ha minst én aktivitet for å kunne fastsette beregningsgrunnlag',
+    );
+    const feltetMåFyllesUtfeilmeldingFørBytteAvTab = await screen.findByText('Feltet må fylles ut');
+    expect(måHaAktivitetFeilmeldingFørBytteAvTab).toBeInTheDocument();
+    expect(feltetMåFyllesUtfeilmeldingFørBytteAvTab).toBeInTheDocument();
+
+    // Bytter tab
+    userEvent.click(screen.getByRole('tab', { name: '13.02.2020' }));
+    userEvent.type(screen.getAllByLabelText('Begrunn endringene')[0], 'Test');
+    userEvent.click(screen.getByRole('button', { name: 'Oppdater' }));
+    const måHaAktivitetFeilmeldingNyTab = screen.queryByText(
+      'Må ha minst én aktivitet for å kunne fastsette beregningsgrunnlag',
+    );
+    const feltetMåFyllesUtfeilmeldingNyTab = screen.queryByText('Feltet må fylles ut');
+    expect(måHaAktivitetFeilmeldingNyTab).not.toBeInTheDocument();
+    expect(feltetMåFyllesUtfeilmeldingNyTab).not.toBeInTheDocument();
+
+    // Byttet tab tilbake til første
+    userEvent.click(screen.getByRole('tab', { name: '13.01.2020' }));
+    const måHaAktivitetFeilmeldingEtterBytteAvTab = await screen.findByText(
+      'Må ha minst én aktivitet for å kunne fastsette beregningsgrunnlag',
+    );
+    const feltetMåFyllesUtfeilmeldingEtterBytteAvTab = await screen.findByText('Feltet må fylles ut');
+    expect(måHaAktivitetFeilmeldingEtterBytteAvTab).toBeInTheDocument();
+    expect(feltetMåFyllesUtfeilmeldingEtterBytteAvTab).toBeInTheDocument();
   });
 });
