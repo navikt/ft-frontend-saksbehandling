f802feac94801b9a6623a7b6fb301a1ccbfc4bc4
tor-nav <tor.roneid@nav.no>
2022-05-11 12:12:14 +0200

    FIX lasting av CSS ved bruk av micro-frontend

--- packages/fakta-beregning/dev/MicroFrontendIndex.tsx
+++ packages/fakta-beregning/dev/MicroFrontendIndex.tsx
@@ -1,4 +1,1 @@
-import '@navikt/ft-ui-komponenter/dist/style.css';
-import '@navikt/ft-form-redux-legacy/dist/style.css';
-
 export { default } from '../src/BeregningFaktaIndex';
--- webpack/rules/style-rule.js
+++ webpack/rules/style-rule.js
@@ -1,25 +1,32 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = (src, testCss) => ({
-  test: testCss ? /\.(less|css)?$/ : /\.(less)?$/,
-  use: [
-    {
+module.exports = (src, shouldTestCss, shouldNotAdd) => {
+  let rules = [{
+    loader: 'css-loader',
+  }, {
+    loader: 'less-loader',
+    options: {
+      lessOptions: {
+        modifyVars: {
+          nodeModulesPath: '~',
+          coreModulePath: '~',
+        },
+      },
+    },
+  }];
+  if (shouldNotAdd === undefined || shouldNotAdd === false) {
+    rules = [{
       loader: MiniCssExtractPlugin.loader,
       options: {
         publicPath: './',
       },
-    }, {
-      loader: 'css-loader',
-    }, {
-      loader: 'less-loader',
-      options: {
-        lessOptions: {
-          modifyVars: {
-            nodeModulesPath: '~',
-            coreModulePath: '~',
-          },
-        },
-      },
-    }],
-  exclude: [src],
-});
+    }].concat(rules);
+  }
+  console.log(rules)
+
+  return {
+    test: shouldTestCss ? /\.(less|css)?$/ : /\.(less)?$/,
+    use: rules,
+    exclude: [src],
+  }
+};
