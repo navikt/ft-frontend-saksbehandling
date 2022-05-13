fbf87572ae88406a5839dcc681da1c595fa15139
Petter Ekern <petter.ekern@nav.no>
2022-05-13 10:53:47 +0200

    FIX: Har gjort fom obligatorisk

--- packages/fakta-beregning/src/BeregningFaktaIndex.stories.tsx
+++ packages/fakta-beregning/src/BeregningFaktaIndex.stories.tsx
@@ -86,6 +86,7 @@ const lagBeregningsgrunnlag = (
     erOverstyrtInntekt: null,
     beregningsgrunnlagPeriode: [
       {
+        beregningsgrunnlagPeriodeFom: '2022-03-02',
         beregningsgrunnlagPrStatusOgAndel: andeler.map(andel => ({
           andelsnr: andel.andelsnr,
           aktivitetStatus: andel.aktivitetStatus,
