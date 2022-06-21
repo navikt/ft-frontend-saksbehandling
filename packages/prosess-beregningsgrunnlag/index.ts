04fe51f859554bac74599d65f590135316c9a4b0
Espen Johansen Velsvik <espen.velsvik@nav.no>
2022-06-21 08:48:35 +0200

    Fikser submit av frilansinntekt og eksporterer submitValueType for prosesspanel

--- packages/prosess-beregningsgrunnlag/index.ts
+++ packages/prosess-beregningsgrunnlag/index.ts
@@ -1,3 +1,4 @@
 export type { default as BeregningsgrunnlagResultatAP } from './src/types/interface/BeregningsgrunnlagAP';
+export type { BeregningAksjonspunktSubmitType } from './src/types/interface/BeregningsgrunnlagAP';
 export { default as ProsessBeregningsgrunnlagAksjonspunktCode } from './src/types/interface/ProsessBeregningsgrunnlagAksjonspunktCode';
 export { default } from './src/BeregningsgrunnlagProsessIndex';
