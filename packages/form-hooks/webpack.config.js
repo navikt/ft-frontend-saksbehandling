2e96e183e9c9303822aab224b9d3e799337691cd
tor-nav <tor.roneid@nav.no>
2022-12-14 14:15:53 +0100

    Disable eslint i webpack.config.js i form-hooks

--- packages/form-hooks/webpack.config.js
+++ packages/form-hooks/webpack.config.js
@@ -1,11 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
-const { merge } = require('webpack-merge');
-const prodConfig = require('../../webpack/webpack.prod.template');
-const styleModulesRule = require('../../webpack/rules/style-modules-rule');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { merge } = require('webpack-merge'); // eslint-disable-line import/no-extraneous-dependencies
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const prodConfig = require('../../webpack/webpack.prod.template'); // eslint-disable-line import/extensions
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const styleModulesRule = require('../../webpack/rules/style-modules-rule'); // eslint-disable-line import/extensions
 
 const SRC_DIR = path.resolve(__dirname, './src');
 
-const config = {
+const config = env => ({
   output: {
     path: path.resolve(__dirname, 'dist'),
     library: {
@@ -13,9 +17,13 @@ const config = {
       type: 'umd',
     },
   },
+  watch: env.watch === 'true',
+  watchOptions: {
+    poll: 1000, // Check for changes every second
+  },
   module: {
     rules: prodConfig.module.rules.concat([styleModulesRule(SRC_DIR)]),
   },
-};
+});
 
-module.exports = merge(prodConfig, config);
+module.exports = env => merge(prodConfig, config(env));
