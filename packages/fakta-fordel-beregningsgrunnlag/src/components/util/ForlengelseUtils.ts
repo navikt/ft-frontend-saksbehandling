0000664a2eb660cde33d32d9dce27de863cd1cd5
Espen Johansen Velsvik <espen.velsvik@nav.no>
2023-01-13 16:25:14 +0100

    Eget ikon for perioder som er vurdert i tidligere behandlinger

--- packages/fakta-fordel-beregningsgrunnlag/src/components/util/ForlengelseUtils.ts
+++ packages/fakta-fordel-beregningsgrunnlag/src/components/util/ForlengelseUtils.ts
@@ -3,15 +3,15 @@ import dayjs from 'dayjs';
 
 function overlapper(fp: ForlengelsePeriodeProp, periode: FordelBeregningsgrunnlagPeriode): boolean {
   return (
-    (dayjs(fp.fom).isSame(periode.fom) || dayjs(fp.fom).isBefore(periode.fom)) &&
-    dayjs(fp.tom).isSame(dayjs(periode.tom) || dayjs(fp.tom).isAfter(periode.tom))
+    (dayjs(fp.fom).isSame(dayjs(periode.fom)) || dayjs(fp.fom).isBefore(dayjs(periode.fom))) &&
+    (dayjs(fp.tom).isSame(dayjs(periode.tom)) || dayjs(fp.tom).isAfter(dayjs(periode.tom)))
   );
 }
 
 function erPeriodeTilVurdering(
   periode: FordelBeregningsgrunnlagPeriode,
   forlengelseperioder?: ForlengelsePeriodeProp[],
-) {
+): boolean {
   return (
     !forlengelseperioder || forlengelseperioder.length === 0 || forlengelseperioder.some(fp => overlapper(fp, periode))
   );
