213bcc686de8b85ec0be3d13a7b69a02627ee94b
Tor Røneid <tor.roneid@nav.no>
2023-02-16 14:19:26 +0100

    Legg til clean-script + fjern unødvendig kode i test-setup (#1100)

--- vitest-setup.ts
+++ vitest-setup.ts
@@ -2,12 +2,3 @@ import matchers from '@testing-library/jest-dom/matchers';
 import { expect } from 'vitest';
 
 expect.extend(matchers);
-
-window.ResizeObserver =
-  window.ResizeObserver ||
-  vi.fn().mockImplementation(() => ({
-    disconnect: vi.fn(),
-    observe: vi.fn(),
-    unobserve: vi.fn(),
-  }));
-  
