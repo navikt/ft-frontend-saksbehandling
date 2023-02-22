0f30ec4e80cc483f73d4dc00976af1a6ad46bd77
Tor Røneid <tor.roneid@nav.no>
2023-02-22 09:19:55 +0100

    Legg til watch-mode på ein del panel for å støtta uvikling via LINK i staden for module federation (#1131)

--- packages/prosess-tilbakekreving-foreldelse/webpack.config.js
+++ packages/prosess-tilbakekreving-foreldelse/webpack.config.js
@@ -1,22 +1,36 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
-const { merge } = require('webpack-merge');
-const prodConfig = require('../../webpack/webpack.prod.template');
-const styleModulesRule = require('../../webpack/rules/style-modules-rule');
-const styleRule = require('../../webpack/rules/style-rule');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { merge } = require('webpack-merge'); // eslint-disable-line import/no-extraneous-dependencies
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const prodConfig = require('../../webpack/webpack.prod.template'); // eslint-disable-line import/extensions
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const devWatchConfig = require('../../webpack/webpack.dev.watch.template'); // eslint-disable-line import/extensions
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const styleModulesRule = require('../../webpack/rules/style-modules-rule'); // eslint-disable-line import/extensions
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const styleRule = require('../../webpack/rules/style-rule'); // eslint-disable-line import/extensions
 
 const SRC_DIR = path.resolve(__dirname, './src');
 
-const config = {
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    library: {
-      name: '@navikt/ft-prosess-tilbakekreving-foreldelse',
-      type: 'umd',
+const config = env => {
+  const prodOrDevConfig = env.watch === 'true' ? devWatchConfig : prodConfig;
+  return {
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      library: {
+        name: '@navikt/ft-prosess-tilbakekreving-foreldelse',
+        type: 'umd',
+      },
     },
-  },
-  module: {
-    rules: prodConfig.module.rules.concat([styleModulesRule(SRC_DIR), styleRule(SRC_DIR)]),
-  },
+    watch: env.watch === 'true',
+    watchOptions: {
+      poll: 1000, // Check for changes every second
+    },
+    module: {
+      rules: prodOrDevConfig.module.rules.concat([styleModulesRule(SRC_DIR), styleRule(SRC_DIR)]),
+    },
+  };
 };
 
-module.exports = merge(prodConfig, config);
+module.exports = env => merge(env.watch === 'true' ? devWatchConfig : prodConfig, config(env));
