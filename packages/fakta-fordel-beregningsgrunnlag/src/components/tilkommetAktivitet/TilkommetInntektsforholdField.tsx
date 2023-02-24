3a834c8f40260718feeb451fd8d5752a8fc04c48
Petter Ekern <petter.ekern@nav.no>
2023-02-24 15:18:11 +0100

    TSF-3224: Hjelpetekster for fordeling

--- packages/fakta-fordel-beregningsgrunnlag/src/components/tilkommetAktivitet/TilkommetInntektsforholdField.tsx
+++ packages/fakta-fordel-beregningsgrunnlag/src/components/tilkommetAktivitet/TilkommetInntektsforholdField.tsx
@@ -1,12 +1,13 @@
+import React, { FC, ReactElement } from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
+import { ReadMore, Label, Alert } from '@navikt/ds-react';
 import { formHooks, InputField, RadioGroupPanel } from '@navikt/ft-form-hooks';
 import { maxValueFormatted, required } from '@navikt/ft-form-validators';
 import { AktivitetStatus } from '@navikt/ft-kodeverk';
 import { Inntektsforhold } from '@navikt/ft-types/src/beregningsgrunnlagFordelingTsType';
 import { VerticalSpacer } from '@navikt/ft-ui-komponenter';
 import { parseCurrencyInput } from '@navikt/ft-utils';
-import React, { FC } from 'react';
 import { ArbeidsgiverOpplysningerPerId } from '@navikt/ft-types';
-import { useIntl } from 'react-intl';
 import { TilkommetAktivitetFormValues } from '../../types/FordelBeregningsgrunnlagPanelValues';
 import styles from './tilkommetAktivitet.less';
 import { getAktivitetNavn } from './TilkommetAktivitetUtils';
@@ -18,6 +19,19 @@ type TilkommetInntektsforholdFieldType = {
   periodeIdentifikator: string;
   inntektsforholdTilVurdering: Inntektsforhold;
   arbeidsgiverOpplysningerPerId: ArbeidsgiverOpplysningerPerId;
+};
+
+const lagHjelpetekst = (inntektsforhold: Inntektsforhold): ReactElement => {
+  switch (inntektsforhold.aktivitetStatus) {
+    case AktivitetStatus.ARBEIDSTAKER:
+      return <FormattedMessage id="BeregningInfoPanel.TilkommetAktivitet.LesMerArbeid" values={{ br: <br /> }} />;
+    case AktivitetStatus.FRILANSER:
+      return <FormattedMessage id="BeregningInfoPanel.TilkommetAktivitet.LesMerFrilans" values={{ br: <br /> }} />;
+    case AktivitetStatus.SELVSTENDIG_NAERINGSDRIVENDE:
+      return <FormattedMessage id="BeregningInfoPanel.TilkommetAktivitet.LesMerNæring" />;
+    default:
+      return <FormattedMessage id="BeregningInfoPanel.TilkommetAktivitet.LesMerArbeid" />;
+  }
 };
 
 export const getInntektsforholdIdentifikator = (inntektsforhold: Inntektsforhold | undefined): string => {
@@ -77,16 +91,28 @@ const TilkommetInntektsforholdField: FC<TilkommetInntektsforholdFieldType> = ({
         validate={[required]}
         isTrueOrFalseSelection
       />
-
+      {skalRedusereUtbetaling === false && (
+        <Alert size="small" variant="info">
+          {intl.formatMessage({ id: 'BeregningInfoPanel.TilkommetAktivitet.Alert' })}
+        </Alert>
+      )}
       {skalRedusereUtbetaling && (
         <>
           <VerticalSpacer sixteenPx />
+          <Label>
+            <FormattedMessage id="BeregningInfoPanel.TilkommetAktivitet.Fastsett" />
+          </Label>
+          <ReadMore header={<FormattedMessage id="BeregningInfoPanel.TilkommetAktivitet.LesMer" />}>
+            {lagHjelpetekst(inntektsforholdTilVurdering)}
+          </ReadMore>
+          <VerticalSpacer eightPx />
           <div className={styles.bruttoInntektContainer}>
             <InputField
               name={`${formName}.${index}.${periodeIdentifikator}.${getInntektsforholdIdentifikator(
                 inntektsforholdTilVurdering,
               )}.bruttoInntektPrÅr`}
               label="Fastsett årsinntekt"
+              hideLabel
               readOnly={readOnly}
               className={styles.bruttoInntektInput}
               parse={parseCurrencyInput}
