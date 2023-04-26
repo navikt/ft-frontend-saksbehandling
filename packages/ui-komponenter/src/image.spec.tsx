4e52648fdac5301bcbf4c8fedde579224b92faa6
tor-nav <tor.roneid@nav.no>
2023-04-26 12:34:36 +0200

    Fjern @storybook/testing-react - Kan bruka @storybook/react direkte

--- packages/ui-komponenter/src/image.spec.tsx
+++ packages/ui-komponenter/src/image.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { composeStories } from '@storybook/testing-react';
+import { composeStories } from '@storybook/react';
 import { Modal } from '@navikt/ds-react';
 import * as stories from './Image.stories';
 
