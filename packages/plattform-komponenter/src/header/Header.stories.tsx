14305d5fc7ba0fc56770fd5dacf96e1d0121f2db
tor-nav <tor.roneid@nav.no>
2023-06-12 15:02:39 +0200

    Fjern navds i plattform-komponenter
    Former-commit-id: caeccf9914d28fa53c31ee30c38b4fef21e2ff12

---
+++ packages/plattform-komponenter/src/header/Header.stories.tsx
@@ -0,0 +1,17 @@
+import React, { ComponentProps } from 'react';
+import { StoryFn } from '@storybook/react';
+import Header from './Header';
+
+export default {
+  title: 'Header',
+  component: Header,
+};
+
+const HeaderTemplate: StoryFn<ComponentProps<typeof Header>> = () => (
+  <Header title="Tittel" titleHref="href">
+    <div style={{ color: 'white' }}>test</div>
+  </Header>
+);
+
+export const Default = HeaderTemplate.bind({});
+Default.args = {};
