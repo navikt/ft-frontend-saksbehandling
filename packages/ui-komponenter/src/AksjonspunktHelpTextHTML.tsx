5f78b90cd29a89401567dc20e545568a28ff7fb3
tor-nav <tor.roneid@nav.no>
2023-10-13 10:53:13 +0200

    FIX feil wrapping i AksjonspunktHelpTextHtml

--- packages/ui-komponenter/src/AksjonspunktHelpTextHTML.tsx
+++ packages/ui-komponenter/src/AksjonspunktHelpTextHTML.tsx
@@ -1,14 +1,7 @@
 import React, { ReactNode, FunctionComponent } from 'react';
 
-import { BodyShort, HStack } from '@navikt/ds-react';
-import { ExclamationmarkTriangleFillIcon } from '@navikt/aksel-icons';
-import { isObject, createIntl } from '@navikt/ft-utils';
-
-import messages from '../i18n/nb_NO.json';
-
-import styles from './aksjonspunktHelpTextHTML.module.css';
-
-const intl = createIntl(messages);
+import { Alert, BodyShort, VStack } from '@navikt/ds-react';
+import { isObject } from '@navikt/ft-utils';
 
 export interface OwnProps {
   children: string[] | ReactNode | ReactNode[];
@@ -17,7 +10,7 @@ export interface OwnProps {
 /**
  * AksjonspunktHelpTextHTML
  *
- * Presentasjonskomponent. Viser hjelpetekster som forteller NAV-ansatt hva som må gjøres for
+ * Viser hjelpetekster som forteller NAV-ansatt hva som må gjøres for
  * å avklare en eller flere aksjonspunkter.
  */
 const AksjonspunktHelpTextHTML: FunctionComponent<OwnProps> = ({ children }) => {
@@ -25,29 +18,17 @@ const AksjonspunktHelpTextHTML: FunctionComponent<OwnProps> = ({ children }) => 
     return null;
   }
 
-  const elementStyle = Array.isArray(children) && children.length > 1 ? styles.severalElements : styles.oneElement;
   return (
-    <div className={styles.container}>
-      <HStack gap="4">
-        <ExclamationmarkTriangleFillIcon
-          title={intl.formatMessage({ id: 'HelpText.Aksjonspunkt' })}
-          height={25}
-          width={25}
-          className={styles.image}
-          color="var(--a-orange-600)"
-        />
-        <div className={styles.aksjonspunktText}>
-          {React.Children.map(children, child => (
-            // @ts-ignore Fiks
-            <div key={isObject(child) ? child.key : child} className={elementStyle}>
-              <BodyShort size="small" className={styles.wordwrap}>
-                {child}
-              </BodyShort>
-            </div>
-          ))}
-        </div>
-      </HStack>
-    </div>
+    <Alert variant="warning">
+      <VStack gap="2">
+        {React.Children.map(children, child => (
+          // @ts-ignore Fiks
+          <BodyShort key={isObject(child) ? child.key : child} size="small">
+            {child}
+          </BodyShort>
+        ))}
+      </VStack>
+    </Alert>
   );
 };
 
