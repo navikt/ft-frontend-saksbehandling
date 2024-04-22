d587fad515304cad7668f7f8b0a16a51e77a817a
Hallvard Andreas Stark <hallvard@scel.to>
2024-04-22 11:57:19 +0200

    Bruker aksel-komponent for ikon

--- packages/plattform-komponenter/src/icons/WarningIcon.tsx
+++ packages/plattform-komponenter/src/icons/WarningIcon.tsx
@@ -1,24 +1,16 @@
+import { ExclamationmarkTriangleFillIcon } from '@navikt/aksel-icons';
 import React from 'react';
 
-const WarningIcon = () => (
-  <svg
-    width="1.5em"
-    height="1.5em"
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    focusable="false"
-    role="img"
+interface WarningIconProps {
+  alt?: string;
+}
+
+const WarningIcon = ({ alt }: WarningIconProps) => (
+  <ExclamationmarkTriangleFillIcon
+    title={alt}
+    fontSize="1.5rem"
     style={{ color: 'var(--ac-alert-icon-warning-color,var(--a-icon-warning))' }}
-  >
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      // eslint-disable-next-line max-len
-      d="M12 2.25a.75.75 0 0 1 .656.387l9.527 17.25A.75.75 0 0 1 21.526 21H2.474a.75.75 0 0 1-.657-1.113l9.526-17.25A.75.75 0 0 1 12 2.25ZM12 8.75a.75.75 0 0 1 .75.75v4a.75.75 0 0 1-1.5 0v-4a.75.75 0 0 1 .75-.75Zm-1 7.75a1 1 0 1 1 2 0 1 1 0 0 1-2 0Z"
-      fill="currentColor"
-    />
-  </svg>
+  />
 );
 
 export default WarningIcon;
