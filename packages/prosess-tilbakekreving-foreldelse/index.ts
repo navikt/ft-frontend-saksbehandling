bd8cad877eeea22a3e7137bfa03cc7760d03aa3c
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2024-06-28 15:00:38 +0200

    Eksporterer typen ForeldelsesresultatActivity (#3165)

--- packages/prosess-tilbakekreving-foreldelse/index.ts
+++ packages/prosess-tilbakekreving-foreldelse/index.ts
@@ -1,3 +1,4 @@
-export type { default as VurderForeldelseAp } from './src/types/VurderForeldelseAp';
 export { default as ForeldelseAksjonspunktCodes } from './src/ForeldelseAksjonspunktCodes';
 export { default as ForeldelseProsessIndex } from './src/ForeldelseProsessIndex';
+export type { default as VurderForeldelseAp } from './src/types/VurderForeldelseAp';
+export type { default as ForeldelsesresultatActivity } from './src/types/foreldelsesresultatActivitytsType';
