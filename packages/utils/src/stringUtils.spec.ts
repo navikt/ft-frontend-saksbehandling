a608f6057009b371f513490a63ff9dcb10f30146
Steffen Pøhner <steffen.h.pohner@nav.no>
2024-09-05 15:25:04 +0200

    legge til flere replace-regler i formaterFritekst

--- packages/utils/src/stringUtils.spec.ts
+++ packages/utils/src/stringUtils.spec.ts
@@ -5,6 +5,11 @@ describe('formaterFritekst', () => {
     ['\uFE58', '-'],
     ['—', '-'],
     ['\t', ' '],
+    ['\u202F', ' '],
+    ['“sitat”', '"sitat"'],
+    ['‘sitat’', "'sitat'"],
+    ['<script>malicious code</script>','&lt;script&gt;malicious code&lt;/script&gt;']
+
   ])(`skal erstatte ugyldige karakter (%s) i tekst`, (fritekst, formatertFritekst) => {
     expect(formaterFritekst(fritekst)).toEqual(formatertFritekst);
   });
