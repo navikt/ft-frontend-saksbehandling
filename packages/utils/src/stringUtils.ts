1653279625aa97af259951b9139288348aa9cd76
Siri Mykland <35257015+sirimykland@users.noreply.github.com>
2024-09-12 11:57:04 +0200

    Fix/fritekstformatering (#3279)
    legge til flere replace-regler i formaterFritekst
    
    Co-authored-by: Steffen Pøhner <steffen.h.pohner@nav.no>

--- packages/utils/src/stringUtils.ts
+++ packages/utils/src/stringUtils.ts
@@ -1,6 +1,9 @@
-// Erstatter ugyldige karakterer limt inn fra Word
 export const formaterFritekst = (value: string | number) =>
   value
     .toString()
-    .replaceAll(/\p{Dash_Punctuation}/gu, '-')
-    .replaceAll('\t', ' ');
+    .replace(/\p{Dash_Punctuation}/gu, '-')
+    .replace(/[\p{Space_Separator}\t]/gu, ' ')
+    .replace(/[“”]/g, '"')
+    .replace(/[‘’`]/g, "'")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
