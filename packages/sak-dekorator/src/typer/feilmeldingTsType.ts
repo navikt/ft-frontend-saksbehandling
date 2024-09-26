bc67a3a98a5d6ddaf2f5422036ca0c7502ef49a7
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2024-09-26 09:35:11 +0200

    Bumper pakker (#3318)

--- packages/sak-dekorator/src/typer/feilmeldingTsType.ts
+++ packages/sak-dekorator/src/typer/feilmeldingTsType.ts
@@ -1,10 +1,8 @@
-type JSONValue = string | number | boolean | JSONObject | JSONArray;
+type JSONValue = string | number | boolean | JSONObject | Array<JSONValue>;
 
 interface JSONObject {
   [x: string]: JSONValue;
 }
-
-interface JSONArray extends Array<JSONValue> {}
 
 type Feilmelding = {
   melding: string;
