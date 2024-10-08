472c03fa0bcce97d5d9c0c494446332c06f341f0
vebnor <vebjorn.nordby@nav.no>
2024-10-08 12:49:09 +0200

    legger tilbake bygg

---
+++ packages/internal/internal-components/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitest/config';
+import { mergeConfig } from 'vite';
+import { dependencies } from './package.json';
+import commonConfig from '../../../vite.config';
+const config = defineConfig({
+  build: {
+    lib: {
+      name: '@ft-frontend-saksbehandling/internal-components',
+    },
+    rollupOptions: {
+      external: Object.keys(dependencies),
+    },
+  },
+});
+export default mergeConfig(commonConfig, config);
