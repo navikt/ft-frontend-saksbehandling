d8cc9b8a4bcb42ceddfc034ac58669e6a4c54202
Hallvard Andreas Stark <hallvard@scel.to>
2024-10-31 13:46:13 +0100

    Fiks test

--- packages/ui-komponenter/src/LoadingPanel.spec.tsx
+++ packages/ui-komponenter/src/LoadingPanel.spec.tsx
@@ -9,6 +9,6 @@ describe('<LoadingPanel>', () => {
   it('skal vise lasteikon', async () => {
     render(<Default />);
 
-    expect(await screen.findByText('venter...')).toBeInTheDocument();
+    expect(await screen.findByText('Venter…')).toBeInTheDocument();
   });
 });
