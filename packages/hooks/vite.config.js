f06db944df64abd95d78bb04929f15326e4be390
Siri Mykland <35257015+sirimykland@users.noreply.github.com>
2024-10-31 13:38:54 +0100

    introduser ft-hooks (#3400)

---
+++ packages/hooks/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config';
+import { mergeConfig } from 'vite';
+import { peerDependencies } from './package.json';
+import commonConfig from '../../vite.config';
+
+const config = defineConfig({
+  build: {
+    lib: {
+      name: '@navikt/ft-hooks',
+    },
+    rollupOptions: {
+      external: Object.keys(peerDependencies),
+    },
+  },
+});
+
+export default mergeConfig(commonConfig, config);
