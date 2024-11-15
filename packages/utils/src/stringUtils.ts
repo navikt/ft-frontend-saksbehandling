5d8140baf2dca507f08cce9f981789784c0d2185
Steffen Pøhner <steffen.h.pohner@nav.no>
2024-11-15 13:32:02 +0100

    Fjerner soft hyphen i fritekstformattering. Tegnet beriker normalt tekst med usynlige metadata om potensielle punkter for orddeling. Backend fpsak aksepterer ikke tegnet.

--- packages/utils/src/stringUtils.ts
+++ packages/utils/src/stringUtils.ts
@@ -2,6 +2,7 @@ export const formaterFritekst = (value: string | number) =>
   value.toString()
     .replace(/\p{Dash_Punctuation}/gu, '-')
     .replace(/[\p{Space_Separator}\t]/gu, ' ')
+    .replace(/\u00AD/g, '')
     .replace(/[“”]/g, '"')
     .replace(/[‘’`]/g, "'")
     .replace(/</g, "&lt;")
