b0a0cf29e7a7607af10459da223a68984e392dfa
Siri Mykland <35257015+sirimykland@users.noreply.github.com>
2024-12-13 10:20:40 +0100

    Feature/oppdater stil på side meny (#3476)
    * redsign av side meny

--- packages/plattform-komponenter/src/side-menu/SideMenu.stories.tsx
+++ packages/plattform-komponenter/src/side-menu/SideMenu.stories.tsx
@@ -1,32 +1,61 @@
-import { StoryFn } from '@storybook/react';
-import React, { ComponentProps } from 'react';
-import WarningIcon from '../icons/WarningIcon';
-import StatefulSideMenuComponent from './StatefulSideMenu';
+import React, { useMemo, useState } from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import SideMenu from './SideMenu';
+import { HStack } from '@navikt/ds-react';
+import { ExclamationmarkTriangleFillIcon } from '@navikt/aksel-icons';
 
-export default {
+const meta = {
   title: 'Side menu',
-  component: StatefulSideMenuComponent,
-};
+  component: SideMenu,
+  render: ({ heading, links, onClick }) => {
+    const [currentIndex, setCurrentIndex] = useState(links.findIndex(({ active }) => active) ?? 0);
+    const handleOnClick = (index: number): void => {
+      setCurrentIndex(index);
+      onClick(index);
+    };
 
-const Template: StoryFn<ComponentProps<typeof StatefulSideMenuComponent>> = () => (
-  <StatefulSideMenuComponent
-    heading="Dette er en test"
-    links={[
+    const linksWithActiveState = useMemo(
+      () =>
+        links.map((link, index) => ({
+          ...link,
+          active: currentIndex === index,
+        })),
+      [links, currentIndex],
+    );
+    return (
+      <div style={{ width: '400px', border: '1px dotted black' }}>
+        <HStack>
+          <SideMenu heading={heading} links={linksWithActiveState} onClick={handleOnClick} />
+          <div style={{ width: '200px', height: '800px', padding: '24px' }}>annet innhold</div>
+        </HStack>
+      </div>
+    );
+  },
+} satisfies Meta<typeof SideMenu>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    heading: 'Fakta om',
+    links: [
       {
-        label: 'en link',
+        label: 'Saken',
       },
       {
-        label: 'en link til',
+        label: 'Medlemskap',
       },
       {
-        icon: <WarningIcon />,
-        label: 'en link med ikon',
+        icon: <ExclamationmarkTriangleFillIcon fontSize="1rem" color={'var(--a-icon-warning)'} />,
+        label: 'Opptjening',
         active: true,
       },
-    ]}
-    onClick={() => null}
-  />
-);
-
-export const StatefulSideMenu = Template.bind({});
-StatefulSideMenu.args = {};
+      {
+        label: 'Arbeid og inntekt',
+      },
+    ],
+    onClick: () => null,
+  },
+};
