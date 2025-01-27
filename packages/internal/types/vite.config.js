3a97cc98bcec36cf33fb46258ae7f685f8136be8
tor-nav <tor.roneid@nav.no>
2025-01-27 13:22:42 +0100

    Flytt pakkene types og kodeverk inn i internal og endre til private

--- packages/internal/types/vite.config.js
+++ packages/internal/types/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vitest/config';
 import { mergeConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
+
+import commonConfig from '../../../vite.config';
+
 import { peerDependencies } from './package.json';
-import commonConfig from '../../vite.config';
 
 const config = defineConfig({
   build: {
