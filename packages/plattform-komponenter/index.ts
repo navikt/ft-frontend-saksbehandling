389360efd5395aa31a0d80095f67fe0f18819684
Tor Røneid <tor.roneid@nav.no>
2025-04-09 09:58:38 +0200

    Fjern ubrukte komponentar frå plattform-komponenter (#3613)

--- packages/plattform-komponenter/index.ts
+++ packages/plattform-komponenter/index.ts
@@ -1,44 +1,29 @@
 /** Vanlige react-komponenter * */
-export { ContentWithTooltip } from './src/ContentWithTooltip/ContentWithTooltip';
-export { DetailView, type DetailViewProps } from './src/DetailView/DetailView';
-export { IndicatorWithOverlay } from './src/IndicatorWithOverlay/IndicatorWithOverlay';
-export { InteractiveList } from './src/InteractiveList/InteractiveList';
 export { Autocomplete } from './src/Autocomplete/Autocomplete';
-export { Box } from './src/box/Box';
-export { Margin } from './src/box/Margin';
-export { Form } from './src/Form/Form';
-export { PageContainer } from './src/PageContainer/PageContainer';
-export { PageError } from './src/PageError/PageError';
-export { BasicList } from './src/BasicList/BasicList';
-export { InfoPanel } from './src/InfoPanel/InfoPanel';
-export { LabelledContent } from './src/LabelledContent/LabelledContent';
-export { LinkButton } from './src/LinkButton/LinkButton';
-export { TitleWithUnderline } from './src/TitleWithUnderline/TitleWithUnderline';
-export { FieldError } from './src/FieldError/FieldError';
+export { ContentWithTooltip } from './src/ContentWithTooltip/ContentWithTooltip';
+export { InteractiveList } from './src/InteractiveList/InteractiveList';
 
-export { ProcessMenu } from './src/ProcessMenu/ProcessMenu';
 export { StepType as ProcessMenuStepType } from './src/ProcessMenu/components/StepType';
+export { ProcessMenu } from './src/ProcessMenu/ProcessMenu';
 
-export { SideMenu } from './src/SideMenu/SideMenu';
-export { PersonCard } from './src/PersonCard/PersonCard';
-export { Gender } from './src/PersonCard/Gender';
-export { EmptyPersonCard } from './src/PersonCard/EmptyPersonCard';
-export { VisittKort } from './src/PersonCard/VisittKort';
-export { GenderIcon } from './src/PersonCard/GenderIcon';
-export { Infostripe } from './src/Infostripe/Infostripe';
-export { SaksbehandlernavnContext } from './src/AssessedBy/SaksbehandlernavnContext';
 export { AssessedBy } from './src/AssessedBy/AssessedBy';
-export { PeriodList } from './src/PeriodList/PeriodList';
+export { SaksbehandlernavnContext } from './src/AssessedBy/SaksbehandlernavnContext';
+export { EmptyPersonCard } from './src/PersonCard/EmptyPersonCard';
+export { Gender } from './src/PersonCard/Gender';
+export { GenderIcon } from './src/PersonCard/GenderIcon';
+export { PersonCard } from './src/PersonCard/PersonCard';
+export { VisittKort } from './src/PersonCard/VisittKort';
+export { SideMenu } from './src/SideMenu/SideMenu';
 
 /** Ikoner * */
 export { ArrowIcon } from './src/icons/ArrowIcon';
-export { BucketIcon } from './src/icons/BucketIcon';
 export { CalendarIcon } from './src/icons/CalendarIcon';
 export { ChevronIconBlack } from './src/icons/ChevronIconBlack';
 export { ChevronIconBlue } from './src/icons/ChevronIconBlue';
 export { ChevronIconGray } from './src/icons/ChevronIconGray';
 export { ChildIcon } from './src/icons/ChildIcon';
 export { DocumentIcon } from './src/icons/DocumentIcon';
+export { DuplicateDocumentsIcon } from './src/icons/DuplicateDocumentsIcon';
 export { EditedBySaksbehandlerIcon } from './src/icons/EditedBySaksbehandlerIcon';
 export { EditIcon } from './src/icons/EditIcon';
 export { ExclamationMarkIcon } from './src/icons/ExclamationMarkIcon';
@@ -51,11 +36,9 @@ export { OnePersonIconBlue } from './src/icons/OnePersonIconBlue';
 export { OnePersonIconGray } from './src/icons/OnePersonIconGray';
 export { OnePersonOutline } from './src/icons/OnePersonOutline';
 export { OnePersonOutlineGray } from './src/icons/OnePersonOutlineGray';
-export { PlusIcon } from './src/icons/PlusIcon';
 export { RedCrossIcon } from './src/icons/RedCrossIcon';
 export { RedCrossIconFilled } from './src/icons/RedCrossIconFilled';
 export { TwoPersonsIcon } from './src/icons/TwoPersonsIcon';
 export { TwoPersonsWithOneHighlightedIconBlue } from './src/icons/TwoPersonsWithOneHighlightedIconBlue';
 export { TwoPersonsWithOneHighlightedIconGray } from './src/icons/TwoPersonsWithOneHighlightedIconGray';
 export { WarningIcon } from './src/icons/WarningIcon';
-export { DuplicateDocumentsIcon } from './src/icons/DuplicateDocumentsIcon';
--- packages/plattform-komponenter/src/box/Margin.ts
+++
@@ -1,7 +0,0 @@
-export enum Margin {
-  small = 'small',
-  medium = 'medium',
-  large = 'large',
-  xLarge = 'xLarge',
-  xxLarge = 'xxLarge',
-}
