656ac0158f6424c5cbf1d9879ca7eba3bc256c89
Siri Mykland <35257015+sirimykland@users.noreply.github.com>
2025-04-23 09:17:19 +0200

    legg til støtte for TIDENES ENDE i periodFormat (#3625)

--- packages/utils/src/periodUtils.spec.ts
+++ packages/utils/src/periodUtils.spec.ts
@@ -1,30 +1,35 @@
 import { expect } from 'vitest';
 
+import { TIDENES_ENDE } from './dateUtils';
 import { periodFormat } from './periodUtils';
 
 const fom = '2020-01-02';
 const tom = '2020-01-04';
 
 describe('periodFormat', () => {
-  it('should formatere ordinær periode', () => {
+  it('skal formatere ordinær periode', () => {
     expect(periodFormat(fom, tom)).toEqual('02.01.2020 - 04.01.2020');
   });
 
-  it('should formatere periode med custom separator', () => {
+  it('skal formatere periode med custom separator', () => {
     expect(periodFormat(fom, tom, { separator: 'til' })).toEqual('02.01.2020 til 04.01.2020');
   });
 
-  it('should formatere periode med custom formatering', () => {
+  it('skal formatere periode med custom formatering', () => {
     expect(periodFormat(fom, tom, { year: '2-digit', month: 'long', day: 'numeric' })).toEqual(
       '2. januar 20 - 4. januar 20',
     );
   });
 
-  it('should formatere periode med tom som er undefined', () => {
+  it('skal formatere periode med tom som er undefined', () => {
     expect(periodFormat(fom, undefined)).toEqual('02.01.2020 - ');
   });
 
-  it('should formatere periode med tom som dagens dato', () => {
+  it('skal formatere periode med tom som dagens dato', () => {
     expect(periodFormat(fom, undefined, { showTodayString: true })).toEqual('02.01.2020 - d.d.');
+  });
+
+  it('skal formatere periode med tom som er tidenes ende (9999-12-31)', () => {
+    expect(periodFormat(fom, TIDENES_ENDE)).toEqual('02.01.2020 - ');
   });
 });
