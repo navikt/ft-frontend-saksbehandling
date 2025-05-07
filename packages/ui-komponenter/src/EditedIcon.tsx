545e2f3a60ef887516c6b6aeba29aca10e543609
Siri Mykland <35257015+sirimykland@users.noreply.github.com>
2025-05-07 14:59:04 +0200

    Feature/legg til size paa radio group panel og check box panel (#3641)
    * legg til size i RadioGroupPanel og CheckBoxPanel
    
    * legg til mellomrom for editedIcon i ReadOnlyField
    
    * fjern unødvendig React import

--- packages/ui-komponenter/src/EditedIcon.tsx
+++ packages/ui-komponenter/src/EditedIcon.tsx
@@ -1,27 +1,12 @@
 import { PersonPencilFillIcon } from '@navikt/aksel-icons';
-import classnames from 'classnames/bind';
 
-import styles from './editedIcon.module.css';
-
-const classNames = classnames.bind(styles);
-
-export interface Props {
-  className?: string;
-}
-
-/*
- * EditedIcon
- *
- * Komponent/Ikon som viser om noe i GUI er endret.
- */
-
-export const EditedIcon = ({ className = '' }: Props) => (
-  <span data-testid="editedIcon" className={classNames('editedIcon', className)}>
+export const EditedIcon = () => (
+  <span data-testid="editedIcon">
     <PersonPencilFillIcon
       title="Saksbehandler har endret feltets verdi"
       height={20}
       width={20}
-      color="var(--a-orange-600)"
+      color="var(--a-icon-warning)"
     />
   </span>
 );
