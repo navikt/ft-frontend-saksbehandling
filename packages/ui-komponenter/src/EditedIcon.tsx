8e5b71da22892897f6461ddb95aa7adc958b3214
Siri Mykland <siri.mykland@nav.no>
2025-05-07 14:41:41 +0200

    legg til mellomrom for editedIcon i ReadOnlyField

--- packages/ui-komponenter/src/EditedIcon.tsx
+++ packages/ui-komponenter/src/EditedIcon.tsx
@@ -1,11 +1,12 @@
 import { PersonPencilFillIcon } from '@navikt/aksel-icons';
 
 export const EditedIcon = () => (
-  <PersonPencilFillIcon
-    data-testid="editedIcon"
-    title="Saksbehandler har endret feltets verdi"
-    height={20}
-    width={20}
-    color="var(--a-icon-warning)"
-  />
+  <span data-testid="editedIcon">
+    <PersonPencilFillIcon
+      title="Saksbehandler har endret feltets verdi"
+      height={20}
+      width={20}
+      color="var(--a-icon-warning)"
+    />
+  </span>
 );
