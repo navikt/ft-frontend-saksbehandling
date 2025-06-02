1c6579a6aa3a5cdc5a8a3682e5ffd15f5ca065c0
tor-nav <tor.roneid@nav.no>
2025-06-02 09:09:08 +0200

    Lagt til darkmode i storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,9 +2,29 @@ import React from 'react';
 
 import { Preview } from '@storybook/react';
 
+import { withThemeDecorator } from '@navikt/ft-frontend-storybook-utils';
+
 import './global.module.css';
 
 import '@navikt/ds-css';
+import '@navikt/ds-css/darkside';
+
+export const globalTypes = {
+  theme: {
+    name: 'Tema',
+    description: 'Aksel tema',
+    defaultValue: 'none',
+    toolbar: {
+      icon: 'circlehollow',
+      items: [
+        { value: 'none', icon: 'close', title: 'Gammel' },
+        { value: 'light', icon: 'circlehollow', title: 'Lys' },
+        { value: 'dark', icon: 'circle', title: 'Mørk' },
+      ],
+      showName: true,
+    },
+  },
+};
 
 const preview: Preview = {
   decorators: [
@@ -13,7 +33,9 @@ const preview: Preview = {
         <Story />
       </div>
     ),
+    withThemeDecorator,
   ],
+  globalTypes,
 };
 
 export default preview;
