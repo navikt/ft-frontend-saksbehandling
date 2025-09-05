d049b25b019f62ce38a006cf1265292a498c25b8
Hallvard Andreas Stark <hallvard@scel.to>
2025-09-05 10:23:01 +0200

    Lager en kopi at kodeverk-typen og eksporterer den

--- packages/prosess-tilbakekreving-foreldelse/index.ts
+++ packages/prosess-tilbakekreving-foreldelse/index.ts
@@ -1,7 +1,7 @@
-export { RelasjonsRolleType } from '@navikt/ft-kodeverk';
 export type { BeregnBeløpParams } from './src/components/splittePerioder/PeriodeController';
 export { ForeldelseAksjonspunktCodes } from './src/ForeldelseAksjonspunktCodes';
 export { ForeldelseProsessIndex } from './src/ForeldelseProsessIndex';
 export type { FeilutbetalingPerioderWrapper } from './src/types/FeilutbetalingPerioder';
 export type { ForeldelsesresultatActivity } from './src/types/ForeldelsesresultatActivity';
+export { RelasjonsRolleType } from './src/types/RelasjonsRolleType';
 export type { VurderForeldelseAp } from './src/types/VurderForeldelseAp';
