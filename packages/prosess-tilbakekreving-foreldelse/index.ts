bf45205c3406ee5b9b0c4f2de8e5ae5e2b768fdc
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2025-09-05 09:32:33 +0200

    Re-exporterer kodeverk fra intern pakke. (#3787)
    Trengs i bruk av denne pakken utenfor repoet.

--- packages/prosess-tilbakekreving-foreldelse/index.ts
+++ packages/prosess-tilbakekreving-foreldelse/index.ts
@@ -1,6 +1,7 @@
+export { RelasjonsRolleType } from '@navikt/ft-kodeverk';
+export type { BeregnBeløpParams } from './src/components/splittePerioder/PeriodeController';
 export { ForeldelseAksjonspunktCodes } from './src/ForeldelseAksjonspunktCodes';
 export { ForeldelseProsessIndex } from './src/ForeldelseProsessIndex';
-export type { VurderForeldelseAp } from './src/types/VurderForeldelseAp';
-export type { ForeldelsesresultatActivity } from './src/types/ForeldelsesresultatActivity';
 export type { FeilutbetalingPerioderWrapper } from './src/types/FeilutbetalingPerioder';
-export type { BeregnBeløpParams } from './src/components/splittePerioder/PeriodeController';
+export type { ForeldelsesresultatActivity } from './src/types/ForeldelsesresultatActivity';
+export type { VurderForeldelseAp } from './src/types/VurderForeldelseAp';
