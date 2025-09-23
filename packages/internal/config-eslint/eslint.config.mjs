cf003ee5804f3080868ff32c3ceec389ed1af1a2
tor røneid <tor.roneid@nav.no>
2025-09-23 17:47:24 +0200

    fix

--- packages/internal/config-eslint/eslint.config.mjs
+++ packages/internal/config-eslint/eslint.config.mjs
@@ -4,7 +4,6 @@ import importPlugin from 'eslint-plugin-import';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
 import pluginReact from 'eslint-plugin-react';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import reactYouMightNotNeedAnEffect from 'eslint-plugin-react-you-might-not-need-an-effect';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import storybook from 'eslint-plugin-storybook';
 import vitest from 'eslint-plugin-vitest';
@@ -36,7 +35,6 @@ export default [
   jsxA11y.flatConfigs.recommended,
   importPlugin.flatConfigs.recommended,
   reactRefresh.configs.vite,
-  reactYouMightNotNeedAnEffect.configs.recommended,
   eslintConfigPrettier,
   {
     rules: {
