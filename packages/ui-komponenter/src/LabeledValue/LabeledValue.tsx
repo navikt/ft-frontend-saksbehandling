1a012105462e00eee077a4ac3cab84338b609a96
Siri Mykland <35257015+sirimykland@users.noreply.github.com>
2025-11-19 15:35:41 +0100

    introduser LabeledValue (#3978)
    * introduser LabeledValue
    
    * fiks .tsx i import
    
    * endre typen til value

---
+++ packages/ui-komponenter/src/LabeledValue/LabeledValue.tsx
@@ -0,0 +1,22 @@
+import type { ReactElement, ReactNode } from 'react';
+
+import { BodyShort, HStack, Label, VStack } from '@navikt/ds-react';
+
+interface Props {
+  label: ReactNode;
+  value: ReactElement | string | number;
+  size?: 'medium' | 'small';
+  fieldType?: 'text' | 'component';
+  horizontal?: boolean;
+}
+
+export const LabeledValue = ({ label, value, horizontal = false, size = 'small', fieldType = 'text' }: Props) => {
+  const StackComponent = horizontal ? HStack : VStack;
+  const gap = horizontal ? 'space-8' : '0';
+  return (
+    <StackComponent gap={gap} align="baseline">
+      <Label size={size}>{label}</Label>
+      {fieldType === 'text' ? <BodyShort size={size}>{value}</BodyShort> : <div>{value}</div>}
+    </StackComponent>
+  );
+};
