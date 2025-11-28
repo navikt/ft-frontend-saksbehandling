9eaef483b64eb160f40f6618306c5e690f0d505f
Siri Mykland <siri.mykland@nav.no>
2025-11-28 11:30:12 +0100

    Updates

--- assets/EditedIcon.stories-BF98QNws.js
+++
@@ -1,4 +0,0 @@
-import{j as t,S as d}from"./iframe-DH7Rhn68.js";import"./preload-helper-PPVm8Dsz.js";const s=()=>t.jsx("span",{"data-testid":"editedIcon",children:t.jsx(d,{title:"Saksbehandler har endret feltets verdi",height:20,width:20,color:"var(--ax-text-neutral)"})});s.__docgenInfo={description:"",methods:[],displayName:"EditedIcon"};const n={},e=()=>t.jsxs("div",{children:["Dette er en tekst",t.jsx(s,{})]});e.__docgenInfo={description:"",methods:[],displayName:"Default"};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`() => <div>
-    Dette er en tekst
-    <EditedIcon />
-  </div>`,...e.parameters?.docs?.source}}};const a=["Default"];export{e as Default,a as __namedExportsOrder,n as default};
