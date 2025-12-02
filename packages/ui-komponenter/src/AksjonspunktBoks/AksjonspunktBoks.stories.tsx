2bc791aced33c1c98779524e786c892e5f9a04bf
Siri Mykland <35257015+sirimykland@users.noreply.github.com>
2025-12-02 12:18:25 +0100

    legg til ny AksjonspunktBoks (#3996)
    * legg til ny AksjonspunktBoks
    
    * fjern ubrukte props
    
    * fiks PR kommentarer
    
    * fiks

---
+++ packages/ui-komponenter/src/AksjonspunktBoks/AksjonspunktBoks.stories.tsx
@@ -0,0 +1,55 @@
+import { TextField, VStack } from '@navikt/ds-react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import { AksjonspunktBoks } from './AksjonspunktBoks';
+
+const meta = {
+  title: 'AksjonspunktBoks',
+  component: AksjonspunktBoks,
+  args: {
+    tittel: 'Dette er en tittel på aksjonspunktboksen',
+    children: (
+      <VStack gap="4">
+        <TextField label="Fødselsdato" htmlSize={20} />
+        <TextField label="Begrunnelse" htmlSize={50} />
+      </VStack>
+    ),
+  },
+} satisfies Meta<typeof AksjonspunktBoks>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const OpprettetAksjonspunkt: Story = {
+  args: {
+    aksjonspunkt: {
+      definisjon: '1234',
+      status: 'OPPR',
+    },
+  },
+};
+
+export const UtførtAksjonspunkt: Story = {
+  args: {
+    aksjonspunkt: {
+      definisjon: '1234',
+      status: 'UTFO',
+    },
+  },
+};
+
+export const ReturnertAksjonspunkt: Story = {
+  args: {
+    aksjonspunkt: {
+      definisjon: '1234',
+      status: 'OPPR',
+    },
+  },
+};
+
+export const UtenAksjonspunkt: Story = {
+  args: {
+    aksjonspunkt: undefined,
+  },
+};
