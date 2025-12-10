d8665456e9c5963166f5cf1e09b3427d3b198eeb
Siri Mykland <siri.mykland@nav.no>
2025-12-10 12:29:36 +0100

    forenkle getFarger

--- packages/prosess-beregning/src/components/sammenligningsgrunnlag/grafFarger.ts
+++ packages/prosess-beregning/src/components/sammenligningsgrunnlag/grafFarger.ts
@@ -1,10 +1,11 @@
 import { InntektAktivitetType } from '@navikt/ft-kodeverk';
 
+type ColorPair = [string, string];
 /**
  * Farger for stolpene i grafen
  * https://aksel.nav.no/grunnleggende/styling/design-tokens#Datavisualisering
  */
-const BAR_COLORS_ARBEID = [
+const BAR_COLORS_ARBEID: ColorPair[] = [
   ['#CCE1FF', '#3386E0'],
   ['#99C4DD', '#3386E0'],
   ['#3386E0', '#005B82'],
@@ -12,16 +13,16 @@ const BAR_COLORS_ARBEID = [
   ['#66CBEC', '#368DA8'],
   ['#005B82', '#368DA8'],
 ];
-const BAR_COLORS_FRILANS = [['#C0B2D2', '#8269A2']];
-const BAR_COLORS_YTELSE = [['#99DEAD', '#2AA758']];
+const BAR_COLORS_FRILANS: ColorPair = ['#C0B2D2', '#8269A2'];
+const BAR_COLORS_YTELSE: ColorPair = ['#99DEAD', '#2AA758'];
 
 export const getBarColors = (inntektAktivitetType: InntektAktivitetType, index: number) => {
   switch (inntektAktivitetType) {
     case InntektAktivitetType.ARBEID:
       return BAR_COLORS_ARBEID[index];
     case InntektAktivitetType.FRILANS:
-      return BAR_COLORS_FRILANS[0];
+      return BAR_COLORS_FRILANS;
     case InntektAktivitetType.YTELSE:
-      return BAR_COLORS_YTELSE[0];
+      return BAR_COLORS_YTELSE;
   }
 };
