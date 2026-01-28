251d6533a9f85f5a79497fd3f2fe1f2d985e532f
Ragnhild Rokkerud Holte <193506135+remhol@users.noreply.github.com>
2026-01-28 09:23:48 +0100

    TFP-6481: HelpText som viser kilde til Beregnet årsinntekt i Avviksberegningen (#4115)
    * Flyttet metoder som brukes flere steder ut i beregningsgrunnlagUtils

---
+++ packages/prosess-beregning/src/components/avviksberegning/avviksberegningUtils.ts
@@ -0,0 +1,70 @@
+import type { BeregningsgrunnlagAndel } from '@navikt/ft-types';
+
+type KildeForAndel = Readonly<{
+  andelsnr: number;
+  arbeidsgiver: string;
+  beregnetPrÅrKilde: 'SAKSBEHANDLER' | 'INNTEKTSMELDING' | 'A_ORDNING' | 'INGEN';
+}>;
+
+export const finnKilderForAndeler = (
+  andeler: BeregningsgrunnlagAndel[],
+  beregningsgrunnlagInntekter: Record<string, number>,
+  formaterVisningsnavnForAndel: (andel: BeregningsgrunnlagAndel) => string,
+): KildeForAndel[] => {
+  return andeler.map(andel => {
+    const arbeidsgiverIdent = andel.arbeidsforhold?.arbeidsgiverIdent;
+    const inntektsmeldingÅrsinntekt = andel.arbeidsforhold?.belopFraInntektsmeldingPrMnd
+      ? andel.arbeidsforhold.belopFraInntektsmeldingPrMnd * 12
+      : undefined;
+    const beregningsgrunnlagÅrsinntekt =
+      arbeidsgiverIdent && beregningsgrunnlagInntekter[arbeidsgiverIdent]
+        ? (beregningsgrunnlagInntekter[arbeidsgiverIdent] / 3) * 12
+        : undefined;
+
+    return {
+      andelsnr: andel.andelsnr,
+      arbeidsgiver: formaterVisningsnavnForAndel(andel),
+      beregnetPrÅrKilde: finnKildeForAndel(andel, inntektsmeldingÅrsinntekt, beregningsgrunnlagÅrsinntekt),
+    };
+  });
+};
+
+/**
+ * Denne metoden finner kilden som er brukt til å utlede beregnerPrAar for andelen.
+ * Verdien fastsattAvSaksbehandler er ikke til å stole på, da den blir satt for alle andeler når én av de blir fastsatt.
+ * Derfor må man sammenlikne beløpene først for å avklare om kilden kan være inntektsmelding/a-ordning, selv om
+ * fastsattAvSaksbehandler kan være true.
+ */
+const finnKildeForAndel = (
+  andel: BeregningsgrunnlagAndel,
+  inntektsmeldingÅrsinntekt?: number,
+  beregningsgrunnlagÅrsinntekt?: number,
+): KildeForAndel['beregnetPrÅrKilde'] => {
+  if (andel.beregnetPrAar === undefined) {
+    return 'INGEN';
+  }
+  if (erLikMedToDesimaler(andel.beregnetPrAar, inntektsmeldingÅrsinntekt)) {
+    return 'INNTEKTSMELDING';
+  }
+  if (erLikMedToDesimaler(andel.beregnetPrAar, beregningsgrunnlagÅrsinntekt)) {
+    return 'A_ORDNING';
+  }
+  if (
+    andel.fastsattAvSaksbehandler &&
+    erBeregnetPrAarOppdatert(andel.beregnetPrAar, inntektsmeldingÅrsinntekt, beregningsgrunnlagÅrsinntekt)
+  ) {
+    return 'SAKSBEHANDLER';
+  }
+  return 'INGEN';
+};
+
+const erBeregnetPrAarOppdatert = (
+  beregnetPrAar: number,
+  inntektsmeldingÅrsinntekt?: number,
+  beregningsgrunnlagÅrsinntekt?: number,
+): boolean =>
+  beregnetPrAar !== 0 || inntektsmeldingÅrsinntekt !== undefined || beregningsgrunnlagÅrsinntekt !== undefined;
+
+const erLikMedToDesimaler = (a: number, b?: number): boolean => {
+  return b !== undefined && a.toFixed(2) === b.toFixed(2);
+};
