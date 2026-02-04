ce78ec7f0e325e01be10fbf0b092b3b3e3d84484
Siri Mykland <35257015+sirimykland@users.noreply.github.com>
2026-02-04 10:22:16 +0100

    fix: fjern viteFinal prop i storybook-config (#4138)
    fix:fjern viteFinal prop i storybook-config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,14 +6,6 @@ import { lagStoriesEntriesForPakker } from './lagStoriesEntriesForPakker.ts';
 const config: StorybookConfig = {
   addons: ['@storybook/addon-links', '@storybook/addon-a11y', '@storybook/addon-docs'],
   framework: '@storybook/react-vite',
-  viteFinal: async c => ({
-    ...c,
-    rules: {
-      test: /\.md$/,
-      use: 'raw-loader',
-    },
-    plugins: (c.plugins ?? []).filter(p => p && 'name' in p && p['name'] !== 'vite:dts'),
-  }),
   stories: async () => ['./*.mdx', ...(await lagStoriesEntriesForPakker('packages'))],
 };
 
